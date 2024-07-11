c1b4e359c57281acf18180aa7cebc8e16511bd44
ALI <fsmaccounting@hotmail.com>
2024-07-11 23:09:57 +0100

    improvments

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -5,19 +5,27 @@ import TrackList from '../TrackList/TrackList';
 
 const Playlist = (props) => {
   const handleNameChange = (event) => {
-
     props.updatePlaylistName(event.target.value);
+  };
+
+  const handleSavePlaylist = () => {
+    props.savePlaylist(); 
+    props.setPlaylistName(''); 
   };
 
   return (
     <div className="Playlist">
-      <input onChange={handleNameChange} placeholder="New Playlist" />
+      <input
+        onChange={handleNameChange}
+        value={props.playlistName}
+        placeholder="New Playlist"
+      />
       <TrackList
         tracks={props.playlistTracks}
         isRemoval={true}
         onRemove={props.removeTrack}
       />
-      <button className="Playlist-save" onClick={props.savePlaylist}>
+      <button className="Playlist-save" onClick={handleSavePlaylist}>
         SAVE TO SPOTIFY
       </button>
     </div>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
-import { toast } from "react-toastify";
-import "./SearchBar.css";
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import './SearchBar.css';
 
 const SearchBar = (props) => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState('');
 
   const handleTermChange = (event) => {
     setTerm(event.target.value);
   };
 
-  const search =() => {
-    if (!term.trim()){
-      toast.error('Enter A Song Title')
-
-      return
+  const handleSearch = () => {
+    if (!term.trim()) {
+      toast.error('Enter a song title');
+      return;
     }
     props.onSearch(term);
+    setTerm(''); 
   };
 
   return (
     <div className="SearchBar">
-      <input placeholder="Enter A Song Title" onChange={handleTermChange} />
-      <button className="SearchButton" onClick={search}>
+      <input
+        placeholder="Enter a song title"
+        value={term}
+        onChange={handleTermChange}
+      />
+      <button className="SearchButton" onClick={handleSearch}>
         SEARCH
       </button>
     </div>
